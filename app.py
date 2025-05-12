8c5a3d1634aca0a8fa603f535ab9f56132dedf2a
Turimikment <mentorturimik@gmail.com>
2025-05-12 23:48:50 +0300

    Update app.py
    нахуй коментарии

--- app.py
+++ app.py
@@ -370,7 +370,7 @@ with get_db() as conn:
                 id SERIAL PRIMARY KEY,
                 start_time TIMESTAMP NOT NULL,
                 location VARCHAR(255) NOT NULL,
-                title VARCHAR(100) NOT NULL UNIQUE  #-- Добавляем уникальное ограничение
+                title VARCHAR(100) NOT NULL UNIQUE  
             )
         ''')
 
