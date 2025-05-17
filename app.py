dd74e9ceaede3bda9884e219763d657343bc1a4c
Turimikment <mentorturimik@gmail.com>
2025-05-17 15:26:16 +0300

    Update app.py
    кнопка возврата

--- app.py
+++ app.py
@@ -233,7 +233,14 @@ swagger_config = {
         "Account": account_model,
         "CreateAccount": create_account_model,
         "UpdateUsername": update_username_model,
-        "AboutMe": about_me_model
+        "AboutMe": about_me_model,
+    "swagger_ui_parameters": {
+        "dom_id": "#swagger-ui",
+        "plugins": [
+            {"src": "/static/swagger-custom.js", "name": "CustomButtonPlugin"}
+        ]
+    },
+    "static_url_path": "/static"
     }
 }
 # ====== Добавить в раздел моделей Swagger ======
