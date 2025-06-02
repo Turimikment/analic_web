6ec04ae025dafbeeabc56bb233718d4922c55f95
Turimikment <mentorturimik@gmail.com>
2025-06-02 21:02:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ db = SQLAlchemy(app)
 
 # Swagger configuration
 SWAGGER_URL = '/swagger'
-API_URL = '/swagger'
+API_URL = '/swagger.json'
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
