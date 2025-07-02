4ce9ce053a98b006b4c06f00ff98854fccefab3a
Turimikment <mentorturimik@gmail.com>
2025-07-03 02:08:56 +0300

    Create main_routes.py

---
+++ routes/main_routes.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, redirect, url_for
+from app.extensions import app
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@bp.route('/main')
+def main_page():
+    return render_template('index.html')
+
+@bp.route('/pipeline')
+def pipeline():
+    return render_template('pipeline.html')
+
+@bp.route('/redis-stats')
+def redis_stats_page():
+    return render_template('redis_stats.html')
+
+@bp.route('/normalization-tutorial')
+def normalization_tutorial():
+    return render_template('normalization_tutorial.html')
+
+@bp.route('/soap-interface')
+def soap_interface():
+    return render_template('soap.html')
+
+@bp.route('/redis-edu')
+def redis_education():
+    return render_template('redis_education.html')
+
+@bp.route('/search')
+def search_page():
+    return render_template('search_holidays.html')
