e65031a2c72b5613f3241fabf8d61b7650c5b6d8
Turimikment <mentorturimik@gmail.com>
2025-07-03 02:47:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import os
 from datetime import datetime
 from redis_utils import CarrotStats
 import db_utils
-from .routes import  main_routes
-from .routes import admin_routes
+from routes import  main_routes
+from routes import admin_routes
 app = Flask(__name__)
 app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'supersecretkey')
