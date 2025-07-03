b6ae3522ed15fa1bc48b3ffe23f4ad7418c36d2d
Turimikment <mentorturimik@gmail.com>
2025-07-03 13:49:47 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+ import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+    
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+    
+    SWAGGER = {
+        "headers": [],
+        "specs": [{"endpoint": "apispec", "route": "/apispec.json"}],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/apidocs/",
+    }
